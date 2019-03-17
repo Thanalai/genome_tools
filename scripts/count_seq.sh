1150322cdbf200345586a5ce10023751b5bf7569
Thanalai Poonsiri <Thanalai@outlook.com>
2019-03-17 12:14:06 -0400

    docs: change

--- scripts/count_seq.sh
+++ scripts/count_seq.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# USAGE: bash count_seq.sh FASTA daniene was here, Hi
+# USAGE: bash count_seq.sh FASTA daniene was here, Hello
 # DESCRIPTION: counts number of seqs in a genome
 
 grep -c ">" $1
