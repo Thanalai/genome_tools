beefcd2c9863f77abd0cf8b47381abc4ad1e8b5a
Thanalai <48610516+Thanalai@users.noreply.github.com>
2019-03-17 12:01:23 -0400

    Update count_seq.sh

--- scripts/count_seq.sh
+++ scripts/count_seq.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# USAGE: bash count_seq.sh FASTA daniene was here
+# USAGE: bash count_seq.sh FASTA daniene was here, Hi
 # DESCRIPTION: counts number of seqs in a genome
 
 grep -c ">" $1
